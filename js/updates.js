864d09b9a19a0a664cc4e394dc795e56ca26056f
alectramell <alectramell@gmail.com>
2017-11-01 17:16:17 -0700

    gchangerz

---
+++ js/updates.js
@@ -0,0 +1,23 @@
+function gameboyAdvance() {
+
+	window.open('gameboyAdvance.txt','Gameboy Advance','nonresizable,height=550,width=600');
+	return false;
+}
+
+function gameboyColor() {
+
+	window.open('gameboyColor.txt','Gameboy Color','nonresizable,height=550,width=600');
+	return false;
+}
+
+function xBox() {
+
+	window.open('xBox.txt','XBOX','nonresizable,height=550,width=600');
+	return false;
+}
+
+function playStation() {
+
+	window.open('playStation.txt','PlayStation','nonresizable,height=550,width=600');
+	return false;
+}
